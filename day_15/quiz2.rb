1f803cee67a1af878cd1c736a1f01b9564abda9d
Jonas Keidel <jonas@jonas-keidel.de>
2020-12-15 18:37:21 +0100

    add day 15
    Signed-off-by: Jonas Keidel <jonas@jonas-keidel.de>

---
+++ day_15/quiz2.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+input_lines = File.readlines("./input.txt").map do |line|
+  line.strip.split(",").map(&:to_i)
+end
+
+input_lines.each do |input|
+  counter = {}
+  input.each.with_index do |number, index|
+    counter[number] = index+1
+  end
+  last_number = 0
+  (input.count+2..30000000).each do |i|
+    c = counter[last_number] || 0
+    counter[last_number] = i-1
+    last_number = c == 0 ? 0 : (i-1) - c
+  end
+  puts "#{input.inspect} - #{last_number}"
+end
